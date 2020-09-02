3d3539aed09671a6d6fda4c8f50d953a22375e36
nicola <s1081898@studenti.univpm.it>
2020-09-02 13:27:58 +0200

    aggiunti bottoni

--- FutsAppXamarin/FutsAppXamarin/home.xaml.cs
+++ FutsAppXamarin/FutsAppXamarin/home.xaml.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using UIKit;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -14,7 +14,23 @@ namespace FutsAppXamarin
     {
         public home()
         {
+        
             InitializeComponent();
+            campo.Source = "campo.jpg";
+            
+            
+        }
+
+        void gestisci_click(object sender, System.EventArgs e)
+        {
+            if(sender.Equals(inserisci))
+                Navigation.PushAsync(new Inserisci());
+            else if (sender.Equals(storico))
+                Navigation.PushAsync(new Storico());
+            else if (sender.Equals(nuova_partita))
+                Navigation.PushAsync(new NuovaPartita());
+            else if (sender.Equals(classifiche))
+                Navigation.PushAsync(new Classifiche());
         }
     }
 }
