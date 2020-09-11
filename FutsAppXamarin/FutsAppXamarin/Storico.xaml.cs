e8816fd22be334d3245c8e877a3afc67b8d7047f
Dell <davide.narcisi98@gmail.com>
2020-09-11 10:10:26 +0200

    continuo case storico

--- FutsAppXamarin/FutsAppXamarin/Storico.xaml.cs
+++ FutsAppXamarin/FutsAppXamarin/Storico.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using FutsAppXamarin.Model;
+using Network;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,9 +14,12 @@ namespace FutsAppXamarin
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class Storico : ContentPage
     {
+        List<Match> PartitaStorico;
         public Storico()
         {
             InitializeComponent();
+            PartitaStorico = new List<Match>(Match.giocate);
+            ListaStorico.ItemsSource = PartitaStorico;
         }
     }
 }
