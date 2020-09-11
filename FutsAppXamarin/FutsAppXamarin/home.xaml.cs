468750ab9e4cf8617c45dc30611e6369f66ae4a9
Dell <davide.narcisi98@gmail.com>
2020-09-11 12:51:50 +0200

    rifnie classi storico, next e inseriscirisutato

--- FutsAppXamarin/FutsAppXamarin/home.xaml.cs
+++ FutsAppXamarin/FutsAppXamarin/home.xaml.cs
@@ -30,7 +30,7 @@ namespace FutsAppXamarin
         void gestisci_click(object sender, System.EventArgs e)
         {
             if (sender.Equals(inserisci))                        
-                Navigation.PushAsync(new Inserisci());            
+                Navigation.PushAsync(new InserisciRisultato());            
             else if (sender.Equals(storico))
                 Navigation.PushAsync(new Storico());
             else if (sender.Equals(nuova_partita))
